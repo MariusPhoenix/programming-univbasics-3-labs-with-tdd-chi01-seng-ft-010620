8c38dca33b499061649e547c1e806cf7e97d0e12
Marius Phoenix <phoenixzenomarius@gmail.com>
2019-12-19 04:07:10 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,10 +2,7 @@ first_number = 1
 second_number = 2
 sum = first_number + second_number
 difference = first_number - second_number
-<<<<<<< HEAD
 product = first_number * second_number
 quotient = first_number / second_number
 
-=======
-product = first_number * second_number
->>>>>>> c51fbadc81cc5435a0f279d50e17618d6401f4d4
+
